ffbf1a35206e754528bc60b5cbcdd68f64f5bbda
masoudnikoofar <masoud.nikoofar@gmail.com>
2023-11-29 13:34:57 +0330

    Changes

--- Database/PGSQL/Queries01.sql
+++ Database/PGSQL/Queries01.sql
@@ -24,6 +24,17 @@ WHERE nspname NOT IN ('pg_catalog', 'information_schema')
 ORDER BY pg_relation_size(C.oid) DESC
 LIMIT 20;
 
+-- OR 
+SELECT nspname || '.' || relname AS "relation",
+    pg_size_pretty(pg_total_relation_size(C.oid)) AS "total_size"
+  FROM pg_class C
+  LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+    AND C.relkind <> 'i'
+    AND nspname !~ '^pg_toast'
+  ORDER BY pg_total_relation_size(C.oid) DESC
+  LIMIT 5;
+
 --Show All PostgreSQL Databases And Their Size
 select
   datname as db,
